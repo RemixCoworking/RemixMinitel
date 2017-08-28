a1c12a23e3a8919acd0d78125bab73ed7fc49045
Julien Bouquillon <julien@revolunet.com>
2017-08-28 14:23:00 +0200

    backup workin term

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,8 +7,9 @@ then
   export LC_ALL=C
 
   stty -F "/dev/ttyUSB0" 1200 istrip cs7 parenb -parodd brkint ignpar icrnl ixon ixany opost onlcr cread hupcl isig icanon echo echoe echok;
-        stty cols 40
+  stty cols 40
   stty rows 24
+  stty -ixon
 
   screen -S minitel -t first bash -c "/home/pi/.nvm/versions/node/v8.4.0/bin/node /home/pi/bl/out.js; bash"
 fi
